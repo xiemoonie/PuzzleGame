0b55aa26c3ff886fddf1b1566e7e86cdf1a72574
Lubomír Baloun <lubosbaloun@gmail.com>
2023-03-10 18:59:12 +0100

    Created main scene with fullscreen border

---
+++ LogicGame1/Scripts/Global/GlobalControls.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public class GlobalControls : Node {
+    public override void _Input(InputEvent eventData) {
+        if (eventData is InputEventKey eventKey) {
+            if (eventKey.Scancode == (ulong)KeyList.Escape) {
+                GetTree().Quit();
+            }
+        }
+    }
+}
