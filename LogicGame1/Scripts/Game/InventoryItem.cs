490a32683a08c835476ac4e5fae736f5ddbae5c4
moonie <carodafcha@gmail.com>
2023-03-24 20:55:01 +0100

    items on inventory

--- LogicGame1/Scripts/Game/InventoryItem.cs
+++ LogicGame1/Scripts/Game/InventoryItem.cs
@@ -4,28 +4,29 @@ using Object = Godot.Object;
 
 public class InventoryItem : Control
 {
-public delegate void PictureOnScreen();
+    public delegate void PictureOnScreen();
 
-public override void _Ready() {
-	   GetParent<ItemOnInventory>().itemAdded(this);
-
-}
-public event PictureOnScreen pictureOnScreenEvent;
-    public void setItem(Sprite s)
+    public override void _Ready() {
+        //  GetParent<ItemOnInventory>().itemAdded(this);
+    }
+    public event PictureOnScreen pictureOnScreenEvent;
+    public void setTexture(Sprite s)
     {
-        GD.Print("some stuff happends");
-        TextureRect item= GetNode<TextureRect>("Content/Texture"); 
+
+        //GD.Print("some stuff happends");
+        TextureRect item = GetNode<TextureRect>("Content/Texture");
         item.Texture = s.Texture;
-        
-        if(pictureOnScreenEvent != null){
-                pictureOnScreenEvent();
-                 GD.Print("chinga wut");
-        }else{
-            GD.Print("say wut");
+
+        if (pictureOnScreenEvent != null) {
+            pictureOnScreenEvent();
+            //      GD.Print("chinga wut");
+        } else {
+            //  GD.Print("say wut");
         }
-        
+
 
     }
+
 }
 
     
